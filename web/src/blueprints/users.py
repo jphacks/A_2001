d0dda66fa6f1ae4fd7df9deaf3fd2a74ffb9799d
daikihashimoto <daiki_hashimoto@shino.ecei.tohoku.ac.jp>
2020-11-01 22:33:24 +0900

    loopを削減等

--- web/src/blueprints/users.py
+++ web/src/blueprints/users.py
@@ -12,21 +12,16 @@ logger = logging.getLogger("app")
 @jwt_required
 def user_exp():
     user_id = get_jwt_identity()
-
     try:
+        payload = request.json
+        level = payload.get("level")
+        title = payload.get("title")
         quests = Quest.query.filter(Quest.user_id == user_id).all()
-        if quests is None:
-            return jsonify({"message": "User's quest not found"}), 404
 
-        d = {}
-        for quest in quests:
-            l = {}
-            l["name"] = quest.content
-            l["exp"] = quest.exp
-            d[quest.id] = l
+        d = {que.id: {"exp": que.exp, "name": que.content} for que in quests}
 
     except Exception as e:
         logger.error(e)
         return jsonify({"message": "Internal server error"}), 500
 
-    return jsonify({"exps": d, "level": 0, "title": " "})
+    return jsonify({"exps": d, "level": level, "title": title})
