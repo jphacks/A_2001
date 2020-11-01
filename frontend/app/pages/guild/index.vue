58354d636c97f9fd387db648f74dcc52cb4919d6
マタタビのねこ <matatabinoneko0721@gmail.com>
2020-11-01 14:11:20 +0900

    ギルドのカードをcomponentにした

--- frontend/app/pages/guild/index.vue
+++ frontend/app/pages/guild/index.vue
@@ -24,34 +24,7 @@
     <b-row>
       <template v-for="(guild, key) in guilds">
         <b-col :key="key" col lg="4" md="4" sm="1">
-          <div>
-            <b-card no-body style="max-width: 20rem">
-              <b-card-body>
-                <b-card-title>quest title</b-card-title>
-                <b-card-text> discription </b-card-text>
-                <div>
-                  <b-form-rating
-                    id="rating-readonly"
-                    class="p-0"
-                    value="3.6536"
-                    readonly
-                    no-border
-                    inline
-                    size="sm"
-                  ></b-form-rating>
-                  <p class="d-inline mb-1 text-muted">100</p>
-                </div>
-                <b-button
-                  type="button"
-                  variant="primary"
-                  size="sm"
-                  :to="{ name: 'guild-id', params: { id: '00' } }"
-                  nuxt
-                  >show more</b-button
-                >
-              </b-card-body>
-            </b-card>
-          </div>
+          <GuildCard :guild="guild" />
         </b-col>
       </template>
     </b-row>
@@ -59,7 +32,11 @@
 </template>
 
 <script>
+import GuildCard from '~/components/guild/GuildCard';
 export default {
+  component: {
+    GuildCard,
+  },
   data: () => {
     return {
       guilds: [
