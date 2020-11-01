62acb16fa050f00939ecd0c231f3a314583cecfd
ishizuka_shota <ishizuka.shota1996@gmail.com>
2020-11-01 10:52:44 +0900

    タスク一覧の表示と編集機能

---
+++ frontend/app/components/quest/SubtaskList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div :id="`subtask_${task.id}`">
+    <b-list-group-item
+      v-for="subtask in task.subtasks"
+      :key="subtask.id"
+      class="subtask d-flex align-items-center"
+      button
+    >
+      <i class="fa fa-circle-o fa-lg"></i>
+      <input
+        v-model="subtask.name"
+        class="subtask-name"
+        placeholder="サブタスクを入力"
+        @blur="updateSubtask(subtask)"
+      />
+    </b-list-group-item>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['task'],
+  methods: {
+    updateSubtask(subtask) {
+      console.log('TODO: サブタスク編集APIへ', subtask);
+    },
+  },
+};
+</script>
+
+<style scoped>
+*:focus {
+  outline: none;
+}
+
+.subtask {
+  text-indent: 1em;
+}
+
+.subtask-name {
+  width: 100%;
+  border: none;
+  background: none;
+  margin-left: 1em;
+}
+</style>
---
+++ frontend/app/pages/quests/_quest.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <b-jumbotron :header="questName" lead="questの説明">
+      <TaskList />
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+import TaskList from '~/components/quest/TaskList';
+
+export default {
+  component: {
+    TaskList,
+  },
+  asyncData({ params }) {
+    return {
+      questName: params.quest,
+    };
+  },
+};
+</script>
