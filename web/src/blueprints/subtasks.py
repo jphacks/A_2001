2da563df2fcb44760a70a860540027984e43ccd9
tunamagur0 <47546832+tunamagur0@users.noreply.github.com>
2020-11-01 14:38:29 +0900

    Add delete subtask endpoint

--- web/src/blueprints/subtasks.py
+++ web/src/blueprints/subtasks.py
@@ -63,3 +63,31 @@ def post_subtask(quest_id, task_id):
 
     response = jsonify(subtask.to_dict())
     return response, 201
+
+
+@subtasks.route("/subtasks/<int:subtask_id>", methods=["DELETE"])
+@jwt_required
+def delete_subtask(quest_id, task_id, subtask_id):
+    user_id = get_jwt_identity()
+
+    try:
+        find_quest_task(user_id, quest_id, task_id)
+    except ValueError as ve:
+        return jsonify({"message": str(ve)}), 404
+    except Exception as e:
+        logger.error(e)
+        return jsonify({"message": "Internal server error"}), 500
+
+    try:
+        subtask = Subtask.query.filter(Subtask.id == subtask_id).first()
+        if subtask is None:
+            return jsonify({"message": "Subtask not found"}), 404
+
+        db.session.delete(subtask)
+        db.session.commit()
+    except Exception as e:
+        logger.error(e)
+        db.session.rollback()
+        return jsonify({"message": "Internal server error"}), 500
+
+    return jsonify({}), 204
