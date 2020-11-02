b76e029af442f1976f02f6a08f977f98bb2ae6c7
tunamagur0 <47546832+tunamagur0@users.noreply.github.com>
2020-11-02 16:13:35 +0900

    Sidebar/Sidebar.vueの不要な要素を削除

--- frontend/app/components/Sidebar/Sidebar.vue
+++ frontend/app/components/Sidebar/Sidebar.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="sidebar">
-    <SidebarHeader />
-    <SidebarForm />
     <nav class="sidebar-nav">
-      <div slot="header"></div>
       <ul class="nav">
         <template v-for="item in navItems">
           <template v-if="item.title">
@@ -17,174 +14,68 @@
           <template v-else-if="item.divider">
             <SidebarNavDivider :key="item.key" :classes="item.class" />
           </template>
-          <template v-else-if="item.label">
-            <SidebarNavLabel
-              :key="item.key"
-              :name="item.name"
-              :url="item.url"
-              :icon="item.icon"
-              :label="item.label"
-              :classes="item.class"
-            />
-          </template>
           <template v-else>
-            <template v-if="item.children">
-              <!-- First level dropdown -->
-              <SidebarNavDropdown
-                :key="item.key"
+            <SidebarNavItem :key="item.key" :classes="item.class">
+              <SidebarNavLink
                 :name="item.name"
                 :url="item.url"
                 :icon="item.icon"
-              >
-                <template v-for="childL1 in item.children">
-                  <template v-if="childL1.children">
-                    <!-- Second level dropdown -->
-                    <SidebarNavDropdown
-                      :key="childL1.key"
-                      :name="childL1.name"
-                      :url="childL1.url"
-                      :icon="childL1.icon"
-                    >
-                      <li
-                        v-for="childL2 in childL1.children"
-                        :key="childL2.key"
-                        class="nav-item"
-                      >
-                        <SidebarNavLink
-                          :name="childL2.name"
-                          :url="childL2.url"
-                          :icon="childL2.icon"
-                          :badge="childL2.badge"
-                          :variant="item.variant"
-                        />
-                      </li>
-                    </SidebarNavDropdown>
-                  </template>
-                  <template v-else>
-                    <!-- eslint-disable -->
-                    <SidebarNavItem :classes="item.class">
-                      <SidebarNavLink
-                        :name="childL1.name"
-                        :url="childL1.url"
-                        :icon="childL1.icon"
-                        :badge="childL1.badge"
-                        :variant="item.variant"
-                      />
-                    </SidebarNavItem>
-                    <!-- eslint-enable -->
-                  </template>
-                </template>
-              </SidebarNavDropdown>
-            </template>
-            <template v-else>
-              <SidebarNavItem :key="item.key" :classes="item.class">
-                <SidebarNavLink
-                  :name="item.name"
-                  :url="item.url"
-                  :icon="item.icon"
-                  :badge="item.badge"
-                  :variant="item.variant"
-                />
-              </SidebarNavItem>
-            </template>
+                :badge="item.badge"
+                :variant="item.variant"
+              />
+            </SidebarNavItem>
           </template>
         </template>
       </ul>
-      <slot></slot>
     </nav>
-    <SidebarFooter />
-    <SidebarMinimizer />
   </div>
 </template>
 <script>
-import SidebarFooter from './SidebarFooter';
-import SidebarForm from './SidebarForm';
-import SidebarHeader from './SidebarHeader';
-import SidebarMinimizer from './SidebarMinimizer';
 import SidebarNavDivider from './SidebarNavDivider';
-import SidebarNavDropdown from './SidebarNavDropdown';
 import SidebarNavLink from './SidebarNavLink';
 import SidebarNavTitle from './SidebarNavTitle';
 import SidebarNavItem from './SidebarNavItem';
-import SidebarNavLabel from './SidebarNavLabel';
 
 export default {
   name: 'Sidebar',
   components: {
-    SidebarFooter,
-    SidebarForm,
-    SidebarHeader,
-    SidebarMinimizer,
     SidebarNavDivider,
-    SidebarNavDropdown,
     SidebarNavLink,
     SidebarNavTitle,
     SidebarNavItem,
-    SidebarNavLabel,
   },
   async fetch() {
     const res = await this.$api
       .$get('/api/quests')
       .catch((err) => console.log(err));
-    console.log(res);
+    const items = [
+      {
+        title: true,
+        name: 'Quest',
+        class: '',
+        wrapper: {
+          element: '',
+          attributes: {},
+        },
+      },
+      { divider: true },
+      ...res.quests.map((e) => ({
+        id: e.id,
+        name: e.content,
+        url: '/quests/' + e.id,
+        badge: {
+          variant: 'primary',
+          text: 3,
+        },
+      })),
+    ];
+    this.navItems = items;
   },
   fetchOnServer: false,
   data() {
     return {
-      navItems: [
-        {
-          title: true,
-          name: 'Quest',
-          class: '',
-          wrapper: {
-            element: '',
-            attributes: {},
-          },
-        },
-        {
-          name: 'Charts',
-          url: '/charts',
-        },
-        {
-          name: 'Charts',
-          url: '/charts',
-        },
-        {
-          name: 'Charts',
-          url: '/charts',
-        },
-        {
-          name: 'Charts',
-          url: '/charts',
-        },
-        {
-          name: 'Charts',
-          url: '/charts',
-        },
-        {
-          name: 'Charts',
-          url: '/charts',
-        },
-        {
-          name: 'Charts',
-          url: '/charts',
-        },
-        {
-          name: 'Charts',
-          url: '/charts',
-        },
-        {
-          name: 'ChartsChartsChartsChartsChartsChartsChartsChartsChartsCharts',
-          url: '/charts',
-        },
-      ],
+      navItems: [],
     };
-  },
-  methods: {
-    handleClick(e) {
-      e.preventDefault();
-      e.target.parentElement.classList.toggle('open');
-    },
   },
 };
 </script>
