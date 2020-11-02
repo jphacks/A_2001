b628dbdf6b861c194d983f639f2134216980d1e9
マタタビのねこ <matatabinoneko0721@gmail.com>
2020-11-02 11:24:18 +0900

    lintが通るように整形

--- frontend/app/plugins/firebase.js
+++ frontend/app/plugins/firebase.js
@@ -1,19 +1,16 @@
-import firebase from 'firebase'
+import firebase from 'firebase';
 
 if (!firebase.apps.length) {
-  firebase.initializeApp(
-    {
-        apiKey: "AIzaSyARAh-X9znEbHAHrl2wyVDvLOUc6GaboO8",
-        authDomain: "a-2001.firebaseapp.com",
-        databaseURL: "https://a-2001.firebaseio.com",
+  firebase.initializeApp({
+    apiKey: 'AIzaSyARAh-X9znEbHAHrl2wyVDvLOUc6GaboO8',
+    authDomain: 'a-2001.firebaseapp.com',
+    databaseURL: 'https://a-2001.firebaseio.com',
     projectId: 'a-2001',
-        storageBucket: "a-2001.appspot.com",
-        messagingSenderId: "507907244331",
-        appId: "1:507907244331:web:42e02dec127493e8e3056e",
-        measurementId: "G-RKETVZ8C7K"
-    
-    })
-
+    storageBucket: 'a-2001.appspot.com',
+    messagingSenderId: '507907244331',
+    appId: '1:507907244331:web:42e02dec127493e8e3056e',
+    measurementId: 'G-RKETVZ8C7K',
+  });
 }
 
-export default firebase
+export default firebase;
