6ad8293b9813ae6a9e559f7193e021b8c14282ad
Kaito Suzuki <kaito@KaitonoMacBook-Pro.local>
2020-11-03 17:03:41 +0900

    add relation for quest exp

--- web/src/models/__init__.py
+++ web/src/models/__init__.py
@@ -1,7 +1,8 @@
 from .user import User
 from .quest import Quest
 from .quest_shared import QuestShared
+from .quest_exp import QuestExp
 from .task import Task
 from .subtask import Subtask
 
-__all__ = ["User", "Quest", "QuestShared", "Task", "Subtask"]
+__all__ = ["User", "Quest", "QuestShared", "QuestExp", "Task", "Subtask"]
