910d3503279bc71e9ad855eaaf75644bfad9702a
tunamagur0 <47546832+tunamagur0@users.noreply.github.com>
2020-11-04 11:22:27 +0900

    lintが通るようにした

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -18,7 +18,6 @@ import {
   Header as AppHeader,
   Sidebar,
   Footer as AppFooter,
-  Breadcrumb,
 } from '~/components/';
 
 export default {
@@ -27,7 +26,6 @@ export default {
     AppHeader,
     Sidebar,
     AppFooter,
-    Breadcrumb,
   },
   data() {
     return {
